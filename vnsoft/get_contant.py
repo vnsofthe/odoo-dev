0f045e25d211d64e2d7c79e0532ec0591ff610e4
vnsofthe <vnsoft.he@gmail.com>
2014-12-11 17:22:48 +0800

    Update

--- vnsoft/get_contant.py
+++ vnsoft/get_contant.py
@@ -34,18 +34,21 @@ def imp(arg1,arg2,arg3):
 req = requests.post("http://api.dangqian.com/apidiqu2/api.asp?format=json&id=000000000000&callback=dict")
 no1 = req.content
 no1 = eval(no1)
+req.close()
 for k,v in no1.get("list").items():
     daima1 = v.get("daima").decode("utf-8").encode("gbk")
 
     req2 = requests.post("http://api.dangqian.com/apidiqu2/api.asp?format=json&id="+daima1+"&callback=dict")
     no2 = req2.content
     no2 = eval(no2)
+    req2.close()
     for k2,v2 in no2.get("list").items():
         daima2 = v2.get("daima").decode("utf-8").encode("gbk")
 
         req3 = requests.post("http://api.dangqian.com/apidiqu2/api.asp?format=json&id="+daima2+"&callback=dict")
         no3 = req3.content
         no3 = eval(no3)
+        req3.close()
         print daima2
         for k3,v3 in  no3.get("list").items():
             imp(no3.get("diming2").decode("utf-8"),no3.get("diming3").decode("utf-8"),v3.get("diming").decode("utf-8"))
