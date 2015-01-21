bda101154c4558c80f65b0a4d69ae0af97cff3f0
vnsofthe <vnsoft.he@gmail.com>
2015-01-21 17:32:26 +0800

    init

--- addons/vnsoft01/vnsoft_stock.py
+++ addons/vnsoft01/vnsoft_stock.py
@@ -25,8 +25,8 @@ class vnsof_stock_change(osv.osv):
         "state":"draft",
         "date":fields.date.today,
         "user_id":lambda obj,cr,uid,context:uid
-
     }
+
 
     def _check_product(self, cr, uid, ids, context=None):
         obj = self.browse(cr, uid, ids[0], context=context)
@@ -63,7 +63,7 @@ class vnsof_stock_change(osv.osv):
 
             product = i.product_from.with_context(location=i.location_from.id)
             if i.from_qty > product.qty_available:
-                raise osv.except_osv(u"错误",u"拆包原产品数量[%d]不可以大于产品在手数量[%d]。" %(i.from_qty,i.product_from.qty_available))
+                raise osv.except_osv(u"错误",u"拆包原产品数量[%d]不能大于产品在手数量[%d]。" %(i.from_qty,i.product_from.qty_available))
             #处理拆包前的产品调整
             inventory_id = inventory_obj.create(cr, uid, {
                 'name': u'拆包: %s' % tools.ustr(i.product_from.name),
@@ -81,27 +81,33 @@ class vnsof_stock_change(osv.osv):
             inventory_line_obj.create(cr , uid, line_data, context=context)
             inventory_obj.action_done(cr, uid, [inventory_id], context=context)
             #处理拆包后产品调整
-            amt = i.product_from.standard_price * i.from_qty
-            if i.product_to.cost_method=="average":
+            amt = i.product_from.standard_price * i.from_qty #拆包前产品的总计成本金额
+            amt_line={} #记录每个子产品拆分后数量*成本价的金额
+            for l in i.line:
+                if l.product_to.cost_method=="average":
+                    amt_line[l.product_to.id] = l.product_to.standard_price * i.from_qty * l.rate
+            for l in i.line:
                 #调整拆包后产品的成本单价
-                new_price = (i.product_to.standard_price * i.product_to.qty_available + amt) / (i.product_to.qty_available+i.to_qty)
-                self.pool.get("product.product").write(cr,uid,i.product_to.id,{"standard_price":new_price})
-            product = i.product_to.with_context(location=i.location_to.id)
-            inventory_id = inventory_obj.create(cr, uid, {
-                'name': u'拆包: %s' % tools.ustr(i.product_to.name),
-                'product_id': i.product_to.id,
-                'location_id': i.location_to.id}, context=context)
-            th_qty = product.qty_available
-            line_data = {
-                'inventory_id': inventory_id,
-                'product_qty': th_qty + i.to_qty,
-                'location_id': i.location_to.id,
-                'product_id': i.product_to.id,
-                'product_uom_id': i.product_to.uom_id.id,
-                'theoretical_qty': th_qty
-            }
-            inventory_line_obj.create(cr , uid, line_data, context=context)
-            inventory_obj.action_done(cr, uid, [inventory_id], context=context)
+                if amt_line.get(l.product_to.id):
+                    #新的单价 = (库存单价*库存数量 + 原产品总成本金额*(子产品拆分成本/总计子产品拆分成本)) / (库存数量 + 子产品拆分数量)
+                    new_price = (l.product_to.standard_price * l.product_to.qty_available + amt*((l.product_to.standard_price*l.rate*i.from_qty)/sum(amt_line.values()))) / (l.product_to.qty_available+l.rate*i.from_qty)
+                    self.pool.get("product.product").write(cr,uid,l.product_to.id,{"standard_price":new_price})
+                product = l.product_to.with_context(location=l.location_to.id)
+                inventory_id = inventory_obj.create(cr, uid, {
+                    'name': u'拆包: %s' % tools.ustr(l.product_to.name),
+                    'product_id': l.product_to.id,
+                    'location_id': l.location_to.id}, context=context)
+                th_qty = product.qty_available
+                line_data = {
+                    'inventory_id': inventory_id,
+                    'product_qty': th_qty + i.from_qty*l.rate,
+                    'location_id': l.location_to.id,
+                    'product_id': l.product_to.id,
+                    'product_uom_id': l.product_to.uom_id.id,
+                    'theoretical_qty': th_qty
+                }
+                inventory_line_obj.create(cr , uid, line_data, context=context)
+                inventory_obj.action_done(cr, uid, [inventory_id], context=context)
         self.write(cr, uid, ids, {'state': 'done'}, context=context)
 
     def create(self, cr, uid, vals, context=None):
@@ -121,3 +127,7 @@ class vnsof_stock_change_line(osv.osv):
         "location_to":fields.many2one("stock.location","To Location",required=True,domain=[('usage', '=', 'internal')]),
         "rate":fields.float("Rate",digits_compute=dp.get_precision('Product Unit of Measure'),required=True,help=u"1个单位原产品可以拆分成多少个单位的子产品？"),
     }
+    _sql_constraints = [
+        ('stock_change_line_uniq', 'unique(change_id,product_to)', u'明细清单中相同产品不能重复!'),
+
+    ]
