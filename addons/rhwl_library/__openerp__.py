443789c1c0cac1753ae789158f05a7286af4a6d1
vnsoft.he@gmail.com <10261121github>
2015-04-24 16:44:45 +0800

    update

---
+++ addons/rhwl_library/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'rhwl_library',
+    'version': '0.1',
+    'category': 'library',
+    'sequence': 16,
+    'summary': '人和未来实验生产管理',
+    'description': """
+人和未来生物科技(长沙)有限公司
+==================================
+实验生产模块
+    """,
+    'author': 'VnSoft',
+    'website': 'https://www.odoo.com/page/crm',
+    # 'images': ['images/Sale_order_line_to_invoice.jpeg','images/sale_order.jpeg','images/sales_analysis.jpeg'],
+    'depends': ['base', 'web','vnsoft_base'],
+    'data': [  ],
+    "qweb":[],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
