5d05f3b1ab27dd6e41c5af6673d29698b5cd7be3
vnsofthe <vnsoft.he@gmail.com>
2015-07-15 00:22:12 +0800

    update

--- addons/rhwl_gene/controllers/web.py
+++ addons/rhwl_gene/controllers/web.py
@@ -16,12 +16,20 @@ import logging
 import os
 import openerp
 import openerp.tools.config as config
+import shutil
 _logger = logging.getLogger(__name__)
 content_m={"0":[("前言",1,1),("致辞",2,2),("个人信息",3,3),("目录",4)],
            "4":[("关于您基因检测报告的说明",8),("关于您健康综合评估和指导建议",13)],
            "8":[("服务项目介绍",8,8),("检测报告解读示例",9,12)],
-           "13":[("肿瘤专项相关疾病预防",14)],
-           "14":[("肿瘤类疾病高危警示",14,14),("肿瘤类疾病易感性综合评估",15,16),("肿瘤类疾病环境风险因素分析与指导",17,18)]
+           "13":[("肿瘤专项相关疾病预防",14),("代谢与营养能力专项相关项目预防",19),("心脑血管疾病专项相关疾病预防",23),("呼吸、消化、泌尿系统疾病相关疾病预防",27),("皮肤肌肉、骨关节、精神和行为障碍相关疾病预防",31),("皮肤、肌肉和骨关节疾病预防",32),("精神和行为障碍专项疾病预防",34)],
+           "14":[("肿瘤类疾病高危警示",14,14),("肿瘤类疾病易感性综合评估",15,16),("肿瘤类疾病环境风险因素分析与指导",17,18)],
+           "19":[("代谢与营养能力相关疾病高危警示",19,19),("代谢与营养能力相关疾病遗传易感性综合评估",20,21),("代谢与营养能力相关疾病环境风险因素分析与指导",22,22)],
+           "23":[("心脑血管类疾病高危警示",23,23),("心脑血管类疾病易感性综合评估",24,24),("心脑血管类疾病环境风险因素分析与指导",25,26)],
+           "27":[("呼吸、消化、泌尿系统疾病高危警示",27,27),("呼吸消化泌尿系统相关疾病遗传易感性综合评估",28,28),("呼吸、消化、泌尿系统疾病环境风险因素分析与指导",29,30)],
+           "31":[("皮肤肌肉、骨关节、精神和行为障碍相关疾病高危警示",31,31)],
+           "32":[("皮肤肌肉和骨关节相关疾病遗传易感性综合评估",32,32),("皮肤肌肉和骨关节疾病相关疾病环境风险因素分析与指导",33,33)],
+           "34":[("精神和行为障碍专项相关疾病遗传易感性综合评估",34,34)]
+
 }
 
 class gene(http.Controller):
@@ -198,6 +206,36 @@ class gene(http.Controller):
                     obj=sample.browse(cr,SUPERUSER_ID,id)
                     if obj.sex=="T":
                         data = content_m.get(str(res.get("params").get("seq")))
+        else:
+            data=res
+
+        response = request.make_response(json.dumps(data,ensure_ascii=False), [('Content-Type', 'application/json')])
+        return response.make_conditional(request.httprequest)
+
+    @http.route("/web/api/genes/report/img/",type="http",auth="none")
+    def _get_rhwl_api_report_img(self,**kw):
+        res = self.check_userinfo(kw)
+        data = []
+        if res.get('statu')==200:
+            registry = RegistryManager.get(request.session.db)
+            with registry.cursor() as cr:
+                sample = registry.get("rhwl.easy.genes")
+                id = sample.search(cr,SUPERUSER_ID,[("name","=",res.get("params").get("id"))])
+                if id:
+                    obj=sample.browse(cr,SUPERUSER_ID,id)
+                    png_path = os.path.join("/data/odoo/file/report",obj.name)
+                    if not os.path.exists(png_path):
+                        os.mkdir(png_path)
+                    ps=int(res.get("params").get("ps"))
+                    pe=int(res.get("params").get("pe"))
+                    for i in range(ps,pe+1):
+                        if not os.path.exists(os.path.join(png_path,"pg_"+str(i).zfill(4)+".pdf")):
+                            shutil.copy(os.path.join("/data/odoo/file/report",obj.name+".pdf"),os.path.join(png_path,obj.name+".pdf"))
+                            os.system("cd "+png_path+";pdftk "+obj.name+".pdf burst")
+                        if not os.path.exists(os.path.join(png_path,"pg_"+str(i).zfill(4)+".png")):
+                            os.system("cd "+png_path+";convert -density 100 pg_"+str(i).zfill(4)+".pdf pg_"+str(i).zfill(4)+".png")
+                        data.append("/rhwl_gene/static/local/report/"+obj.name.encode("utf-8")+"/pg_"+str(i).zfill(4)+".png")
+
         else:
             data=res
 
