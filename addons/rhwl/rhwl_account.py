169fd15a27da07fa06e732f3525ae1eb65f86776
vnsoft.he@gmail.com <10261121github>
2015-08-10 17:44:05 +0800

    update

--- addons/rhwl/rhwl_account.py
+++ addons/rhwl/rhwl_account.py
@@ -13,3 +13,32 @@ class vnsoft_account(osv.osv):
     _columns={
         "page_inv_no":fields.char(u"纸质发票号"),
     }
+
+class rhwl_material(osv.osv):
+    _name = "rhwl.material.cost"
+    _columns={
+        "date":fields.date("Cost Date",required=True),
+        "user_id":fields.many2one("res.users",string="User",readonly=True),
+        "compute_date":fields.datetime("Compute Time",readonly=True),
+        "state":fields.selection([("draft","Draft"),("done","Done")]),
+    }
+    _defaults={
+        "user_id":lambda obj,cr,uid,context:uid,
+        "state":"draft"
+    }
+
+class rhwl_material_line(osv.osv):
+    _name="rhwl.material.cost.line"
+    _columns={
+        "parent_id":fields.many2one("rhwl.material.cost","Parent"),
+        "data_kind":fields.selection([("begin","Begin"),("this","This"),("end","End")],string="Data Kind"),
+        "product_id":fields.many2one("product.product","Product",required=True),
+        "brand":fields.related("product_id","brand",type="char",string=u"品牌",readonly=True),
+        "default_code":fields.related("product_id","default_code",type="char",string=u"货号",readonly=True),
+        "attribute":fields.related("product_id","attribute_value_ids",obj="product.attribute.value", type="many2many",string=u"规格",readonly=True),
+        "uom_id":fields.related("product_id","uom_id",type="many2one",obj="product.uom",string="Unit",readonly=True),
+        "qty":fields.float("Qty",digits_compute=dp.get_precision('Product Unit of Measure'),required=True,),
+        'price': fields.float('Price',digits_compute= dp.get_precision('Product Price'), readonly=True,),
+        "project":fields.many2one("res.company.project","Project"),
+        "is_rd":fields.boolean("R&D"),
+    }
