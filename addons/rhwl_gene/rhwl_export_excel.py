c4cd006dcc2d358b09708ed7050fc3d39fe69def
Administrator <vnsoft.he@gmail.com>
2015-11-22 21:44:02 +0800

    update

--- addons/rhwl_gene/rhwl_export_excel.py
+++ addons/rhwl_gene/rhwl_export_excel.py
@@ -52,6 +52,8 @@ class rhwl_export_excel(osv.osv_memory):
             context={}
         if context.get('func_name','')=='informat':
             return self.action_excel_informat(cr,uid,ids,context=context)
+        elif context.get('func_name','')=='informat_new':
+            return self.action_excel_informat_new(cr,uid,ids,context=context)
         elif context.get('func_name','')=='dna':
             return self.action_excel_dna(cr,uid,ids,context=context)
         elif context.get('func_name','')=="snp":
@@ -370,6 +372,107 @@ class rhwl_export_excel(osv.osv_memory):
         if not os.path.exists(u"/data/odoo/file/upload/tjs/样本问题反馈"):
             os.mkdir(u"/data/odoo/file/upload/tjs/样本问题反馈")
         t_dir=u"/data/odoo/file/upload/tjs/样本问题反馈/"+file_str+u"邮寄样本问题反馈"
+        if not os.path.exists(t_dir):
+            os.mkdir(t_dir)
+
+        for i in os.listdir(xlsname):
+            if os.path.exists(os.path.join(t_dir,i.decode('utf-8'))):
+                if os.path.isdir(os.path.join(t_dir,i.decode('utf-8'))):
+                    shutil.rmtree(os.path.join(t_dir,i.decode('utf-8')))
+                else:
+                    os.remove(os.path.join(t_dir,i.decode('utf-8')))
+            shutil.move(os.path.join(xlsname,i.decode('utf-8')),os.path.join(t_dir,i.decode('utf-8')))
+
+        id = self.create(cr,uid,{"state":"netdisk",})
+
+        os.system("rm -Rf "+xlsname)
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'rhwl.gene.export.excel',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': id,
+            'views': [(False, 'form')],
+            'target': 'new',
+            'name':u"导出样本问题反馈Excel"
+        }
+
+    def action_excel_informat_new(self,cr,uid,ids,context=None):
+        if not context.get("active_ids"):return
+        fileobj = NamedTemporaryFile('w+',delete=True)
+        xlsname =  fileobj.name
+        fileobj.close()
+        if not os.path.exists(xlsname):
+            os.mkdir(xlsname)
+
+        ids=context.get("active_ids")
+        if isinstance(ids,(list,tuple)):
+            ids.sort()
+
+        w = xlwt.Workbook(encoding='utf-8')
+        ws = w.add_sheet(u"样本问题反馈")
+        ws.write(0,0,u"序号",style=self.get_excel_style(font_size=11))
+        ws.write(0,1,u"基因样品编码",style=self.get_excel_style(font_size=11)),
+        ws.write(0,2,u"姓名",style=self.get_excel_style(font_size=11)),
+        ws.write(0,3,u"性别",style=self.get_excel_style(font_size=11)),
+        ws.write(0,4,u"身份证号",style=self.get_excel_style(font_size=11)),
+        ws.write(0,5,u"备注",style=self.get_excel_style(font_size=11)),
+        ws.write(0,6,u"手机号",style=self.get_excel_style(font_size=11)),
+        ws.write(0,7,u"会员部反馈",style=self.get_excel_style(font_size=11))
+        ws.col(1).width = 4500 #1000 = 3.14(Excel)
+        ws.col(4).width = 7000
+        ws.col(5).width = 8000
+        ws.col(6).width = 6000
+        rows=1
+        seq=1
+        old_date=""
+        file_s=""
+        file_e=""
+        for i in self.pool.get("rhwl.easy.genes.new").browse(cr,uid,ids,context=context):
+            if not file_s:
+                file_s=i.date.split("-")
+                file_s=str(int(file_s[1]))+"."+str(int(file_s[2]))
+
+            if i.date != old_date:
+                old_date = i.date.split("-")
+                ws.write(rows,0,str(int(old_date[1]))+u"月"+str(int(old_date[2]))+u"日")
+                rows += 1
+                seq = 1
+            image_dir=".".join([str(int(k)) for k in i.date.split("-")[1:]]) + u"日邮寄样本问题反馈图片"
+
+            if i.img_new:
+                if not os.path.exists(os.path.join(xlsname,image_dir)):
+                    os.mkdir(os.path.join(xlsname,image_dir))
+                image_file = os.path.join(os.path.join(xlsname,image_dir),i.name+i.cust_name+u".png")
+                f=open(image_file,'wb')
+                f.write(base64.decodestring(i.img_new))
+                f.close()
+
+            ws.write(rows,0,seq,style=self.get_excel_style(font_size=11,horz=xlwt.Alignment.HORZ_CENTER))
+            ws.write(rows,1,i.name,style=self.get_excel_style(font_size=11))
+            ws.write(rows,2,i.cust_name,style=self.get_excel_style(font_size=11))
+            ws.write(rows,3,u"男" if i.sex==u"T" else u"女",style=self.get_excel_style(font_size=11))
+            ws.write(rows,4,True and i.identity or "",style=self.get_excel_style(font_size=11))
+            ws.write(rows,5,i.except_note,style=self.get_excel_style(font_size=11))
+            ws.write(rows,6,True and i.mobile or "",style=self.get_excel_style(font_size=11))
+            ws.write(rows,7,True and i.confirm_note or "",style=self.get_excel_style(font_size=11))
+            rows +=1
+            seq += 1
+            old_date=i.date
+
+            file_e=i.date.split("-")
+            file_e=str(int(file_e[1]))+"."+str(int(file_e[2]))
+
+        if file_s==file_e:
+            file_str=file_s
+        else:
+            file_str = file_s+"-"+file_e
+
+        w.save(os.path.join(xlsname,file_str+u"邮寄样本问题反馈.xls"))
+
+        if not os.path.exists(u"/data/odoo/file/upload/yg/样本问题反馈"):
+            os.mkdir(u"/data/odoo/file/upload/yg/样本问题反馈")
+        t_dir=u"/data/odoo/file/upload/yg/样本问题反馈/"+file_str+u"邮寄样本问题反馈"
         if not os.path.exists(t_dir):
             os.mkdir(t_dir)
 
